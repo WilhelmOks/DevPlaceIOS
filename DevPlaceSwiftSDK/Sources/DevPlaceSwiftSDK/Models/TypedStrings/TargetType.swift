c8beb59e74d15ee9e6bd3248c4cafbc68a4e144d
Wilhelm Oks <oksw@Mac.fritz.box>
2026-07-24 14:53:17 +0200

    adding comment TargetType

--- DevPlaceSwiftSDK/Sources/DevPlaceSwiftSDK/Models/TypedStrings/TargetType.swift
+++ DevPlaceSwiftSDK/Sources/DevPlaceSwiftSDK/Models/TypedStrings/TargetType.swift
@@ -1,5 +1,6 @@
 public enum TargetType: String, Hashable, Sendable {
     case post = "post"
+    case comment = "comment"
     case project = "project"
     case news = "news"
     case issue = "issue"

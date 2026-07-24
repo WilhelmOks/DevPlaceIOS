686050f8993a9414488f06cb69158c1b56fdf149
Wilhelm Oks <oksw@Mac.fritz.box>
2026-07-24 15:36:58 +0200

    indent guide line adjustment

--- DevPlaceIOS/UI/Components/Comment/CommentView.swift
+++ DevPlaceIOS/UI/Components/Comment/CommentView.swift
@@ -62,8 +62,8 @@ struct CommentView: View {
 private struct VerticalLine: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         return path
     }
 }

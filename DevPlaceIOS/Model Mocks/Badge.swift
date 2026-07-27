0b2f7c499e8e1eb26d3f0269e666609593af3e39
Wilhelm Oks <oksw@Mac.fritz.box>
2026-07-27 13:42:45 +0200

    adding icon to Badges in Profile

--- DevPlaceIOS/Model Mocks/Badge.swift
+++ DevPlaceIOS/Model Mocks/Badge.swift
@@ -6,22 +6,27 @@ extension Collection where Element == Badge {
         [
             .init(
                 name: "Early Adopter",
+                icon: "🌱",
                 createdAt: Date().addingTimeInterval(-60 * 60 * 24 * 365),
             ),
             .init(
                 name: "Top Contributor",
+                icon: "🏆",
                 createdAt: Date().addingTimeInterval(-60 * 60 * 24 * 90),
             ),
             .init(
                 name: "Bug Hunter",
+                icon: "🐛",
                 createdAt: Date().addingTimeInterval(-60 * 60 * 24 * 30),
             ),
             .init(
                 name: "Star Gazer",
+                icon: "⭐️",
                 createdAt: Date().addingTimeInterval(-60 * 60 * 24 * 7),
             ),
             .init(
                 name: nil,
+                icon: nil,
                 createdAt: Date().addingTimeInterval(-60 * 60),
             ),
         ]
--- DevPlaceSwiftSDK/Sources/DevPlaceSwiftSDK/Models/Response/Badge.swift
+++ DevPlaceSwiftSDK/Sources/DevPlaceSwiftSDK/Models/Response/Badge.swift
@@ -2,13 +2,16 @@ import Foundation
 
 public struct Badge: Hashable, Sendable {
     public let name: String?
+    public let icon: String?
     public let createdAt: Date
 
     public init(
         name: String?,
+        icon: String?,
         createdAt: Date,
     ) {
         self.name = name
+        self.icon = icon
         self.createdAt = createdAt
     }
 }
@@ -16,6 +19,7 @@ public struct Badge: Hashable, Sendable {
 extension Badge {
     struct CodingData: Decodable {
         let name: String?
+        let icon: String?
         let created_at: Date
     }
 }
@@ -24,6 +28,7 @@ extension Badge.CodingData {
     var decoded: Badge {
         .init(
             name: name,
+            icon: icon,
             createdAt: created_at,
         )
     }

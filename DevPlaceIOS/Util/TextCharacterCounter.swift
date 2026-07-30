9bd0eb30d603670ef39d357a8a92cc34b16d7c91
Wilhelm Oks <oksw@Mac.fritz.box>
2026-07-30 21:02:10 +0200

    changed comment about the character counting

--- DevPlaceIOS/Util/TextCharacterCounter.swift
+++ DevPlaceIOS/Util/TextCharacterCounter.swift
@@ -1,5 +1,5 @@
 enum TextCharacterCounter {
     static func numberOfCharacters(_ text: String) -> Int {
-        text.utf16.count // "wrong" counting, to replicate how the web app and the backend is counting.
+        text.utf16.count // "wrong" counting, to replicate how the web app is counting. Turns out that the backend is counting differently. Waiting to be fixed...
     }
 }
